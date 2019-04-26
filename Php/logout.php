b154a85243de925e1e032ef26932af93530befb9
SanandaB <34948941+SanandaB@users.noreply.github.com>
2019-04-26 09:38:25 -0600

    Update logout.php

--- Php/logout.php
+++ Php/logout.php
@@ -1,3 +1,11 @@
+
+<!--
+Logout.php basically has a php code segment to carry out the logout functionality all across the website. 
+It is achieved using session_destroy() method.
+Logout.php is linked to the Signout link on every web page that have the sign out option in the dropdown of username in the navigation bar.
+--!>
+
+
 <?php
 	session_start();
 	session_destroy();
